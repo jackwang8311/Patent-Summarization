d09502ca6c691d8b5a709c8207c116b377ba09a3
jackwang8311 <71335037+jackwang8311@users.noreply.github.com>
2020-12-28 19:02:52 +0800

    Add files via upload

---
+++ Summarization/urls.py
@@ -0,0 +1,29 @@
+"""Patent URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import include, path
+from . import views
+
+app_name = 'Summarization'
+
+urlpatterns = [
+    path('', views.home, name='index'),
+    path('uploaded/', views.Submit, name='Submit'),
+    path('download/', views.download, name='download'),
+    path('result/', views.result, name='result'),
+    #path('output/', views.handle_uploaded_file, name='uploaded'),
+    
+]
